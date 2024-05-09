a6f2b79cb6115007444b370d90f33e90b237fb96
hahyunMoon <109493357+hahyunMoon@users.noreply.github.com>
2024-05-09 14:30:10 +0900

    Title: 특정 위치의 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240509/특정 위치의 숫자/numbers-in-specific-location.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int i, a, sum=0;
+    for(i=0;i<10;i++){
+        scanf("%d ", &a);
+        sum += a;
+    }
+    printf("%d",sum);
+    return 0;
+}
