4b122e71d701137b518e53ab59635c9f26eb915a
hahyunMoon <109493357+hahyunMoon@users.noreply.github.com>
2024-05-10 12:01:17 +0900

    Title: 대문자로 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240510/대문자로 바꾸기/change-to-capital.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char arr[5][3] = {0,};
+    for(int i=0; i<5; i++){
+        for(int j=0; j<3; j++){
+            scanf(" %c", &arr[i][j]);
+        }
+    }
+    for(int i=0; i<5; i++){
+        for(int j=0; j<3; j++){
+            printf("%c ", arr[i][j]-32);
+        }
+        printf("\n");
+    }
+    return 0;
+}
