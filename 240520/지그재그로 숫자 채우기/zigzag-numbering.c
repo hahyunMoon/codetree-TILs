a218c34fd486324ed7472d057423ed29848143f2
hahyunMoon <109493357+hahyunMoon@users.noreply.github.com>
2024-05-20 15:19:11 +0900

    Title: 지그재그로 숫자 채우기, Time: 13ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240520/지그재그로 숫자 채우기/zigzag-numbering.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int i , j, n, m, num=0, arr[10][10];
+
+    scanf("%d %d", &n,&m); //입력받기
+
+    for(i =0;i<m;i++){
+        if(i %2 == 0){
+            for(j=0;j<n;j++) arr[j][i] = num++;
+        }
+        else for(j=n-1;j>=0;j--) arr[j][i] = num++;
+    }
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
