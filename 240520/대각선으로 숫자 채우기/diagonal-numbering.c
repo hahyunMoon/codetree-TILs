a6a7c48a73aa89bfd595e35e63ffdf54f5eb2092
hahyunMoon <109493357+hahyunMoon@users.noreply.github.com>
2024-05-20 15:50:48 +0900

    Title: 대각선으로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240520/대각선으로 숫자 채우기/diagonal-numbering.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    int i , j, n, m, num=1, arr[100][100];
+
+    scanf("%d %d", &n,&m); //입력받기
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            if(i==0){
+                if(j==0) arr[i][j] = 1;
+                else arr[i][j]= arr[i][j-1] + num++;
+            }
+            else if(j==m-1 && i>0){
+                arr[i][j] = arr[i-1][j] + num--;
+            }
+            else{
+                arr[i][j] = arr[i-1][j+1] +1;
+            }
+        }
+    }
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
